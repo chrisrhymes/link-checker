cadd4bf0023b8cad1c72934fc6ac2dd4b4f4eb3a
Chris Rhymes <chris.rhymes@westnorthants.gov.uk>
2023-02-09 10:40:26 +0000

    Add model properties

--- src/Models/BrokenLink.php
+++ src/Models/BrokenLink.php
@@ -4,7 +4,21 @@ namespace ChrisRhymes\LinkChecker\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
+/**
+ * ChrisRhymes\LinkChecker\Models\BrokenLink
+ * 
+ * @property int $id
+ * @property int $linkable_id
+ * @property string $linkable_type
+ * @property string $broken_link
+ * @property string $link_text
+ * @property string $exception_message
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Model|\Eloquent|null $linkable
+ */
 class BrokenLink extends Model
 {
     use HasFactory;
@@ -17,7 +31,7 @@ class BrokenLink extends Model
         'exception_message',
     ];
 
-    public function linkable()
+    public function linkable(): MorphTo
     {
         return $this->morphTo();
     }
